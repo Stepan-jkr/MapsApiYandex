19076402143f980c6f2c1e2396bbfee49e348df8
Stepan <stepan.tom@yandex.ru>
2020-02-13 14:14:17 +0300

    Часть 1

---
+++ Vol1.py
@@ -0,0 +1,34 @@
+import os
+import sys
+
+import pygame
+import requests
+
+response = None
+map_request = "http://static-maps.yandex.ru/1.x/?l=sat,skl&ll=37.698866%2C55.736997&z=10"
+response = requests.get(map_request)
+
+if not response:
+    print("Ошибка выполнения запроса:")
+    print(map_request)
+    print("Http статус:", response.status_code, "(", response.reason, ")")
+    sys.exit(1)
+
+# Запишем полученное изображение в файл.
+map_file = "map.png"
+with open(map_file, "wb") as file:
+    file.write(response.content)
+
+# Инициализируем pygame
+pygame.init()
+screen = pygame.display.set_mode((600, 450))
+# Рисуем картинку, загружаемую из только что созданного файла.
+screen.blit(pygame.image.load(map_file), (0, 0))
+# Переключаем экран и ждем закрытия окна.
+pygame.display.flip()
+while pygame.event.wait().type != pygame.QUIT:
+    pass
+pygame.quit()
+
+# Удаляем за собой файл с изображением.
+os.remove(map_file)
